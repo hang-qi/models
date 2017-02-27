cd671a3e0d4c51459e6ba05845f71d66f30fa77e
Hang Qi <qih@qihang.im>
2017-02-27 15:27:23 -0800

    Output loss.

--- inception/inception/inception_train.py
+++ inception/inception/inception_train.py
@@ -338,12 +338,12 @@ def train(dataset):
 
       assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
 
-      if step % 10 == 0:
-        examples_per_sec = FLAGS.batch_size / float(duration)
-        format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                      'sec/batch)')
-        print(format_str % (datetime.now(), step, loss_value,
-                            examples_per_sec, duration))
+      #if step % 10 == 0:
+      examples_per_sec = FLAGS.batch_size / float(duration)
+      format_str = ('%s: step %d, loss = %.6f (%.1f examples/sec; %.3f '
+                    'sec/batch)')
+      print(format_str % (datetime.now(), step, loss_value,
+                          examples_per_sec, duration))
 
       if step % 100 == 0:
         summary_str = sess.run(summary_op)
