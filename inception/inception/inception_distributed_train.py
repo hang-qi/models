ec6049a1b1b2d3b173bd6e16e77471a04a147c4c
Hang Qi <qih@qihang.im>
2017-03-23 23:49:04 -0700

    Stop at max steps

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -281,8 +281,6 @@ def train(target, dataset, cluster_spec, num_tasks):
           start_time = time.time()
           loss_value, step = sess.run([train_op, global_step])
           assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
-          if step > FLAGS.max_steps:
-            break
           duration = time.time() - start_time
 
           #if step % 30 == 0:
@@ -301,6 +299,9 @@ def train(target, dataset, cluster_spec, num_tasks):
 
             # Determine the next time for running the summary.
             next_summary_time += FLAGS.save_summaries_secs
+
+          if step == FLAGS.max_steps:
+            break
         except:
           # if is_chief:
           #   tf.logging.info('About to execute sync_clean_up_op!')
