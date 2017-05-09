adcb6b63b6a6439877d5e3343ec59c360f26dc89
Hang Qi <qih@qihang.im>
2017-05-09 14:29:38 -0700

    Experimental: simple cvx.

--- inception/inception/cifar10.py
+++ inception/inception/cifar10.py
@@ -153,7 +153,7 @@ def _variable_with_weight_decay(name, shape, stddev, wd):
       name,
       shape,
       tf.truncated_normal_initializer(stddev=stddev, dtype=dtype))
-  if wd is not None:
+  if wd is not None and wd > 0:
     weight_decay = tf.multiply(tf.nn.l2_loss(var), wd, name='weight_loss')
     tf.add_to_collection(LOSSES_COLLECTION, weight_decay)
   return var
@@ -198,6 +198,127 @@ def inputs(eval_data):
     images = tf.cast(images, tf.float16)
     labels = tf.cast(labels, tf.float16)
   return images, labels
+
+
+def cvx_inference(images, num_class=NUM_CLASSES, for_training=False,
+              restore_logits=True, scope=None):
+  """A super simple convex linear + softmax model."""
+  def linear(inp, out_dim, scope_name='linear'):
+    with tf.variable_scope(scope_name) as scope:
+      in_dim = inp.get_shape()[1].value
+      weights = _variable_with_weight_decay('weights', [in_dim, out_dim],
+                                            stddev=1/float(in_dim), wd=0.0)
+      biases = _variable_on_cpu('biases', [out_dim], tf.constant_initializer(0.0))
+      linear = tf.add(tf.matmul(inp, weights), biases, name=scope.name)
+      _activation_summary(linear)
+    return linear
+
+  with tf.name_scope(scope, 'simple_linear_softmax', [images]) as sc:
+    reshaped = tf.reshape(images, [images.get_shape()[0].value, -1])
+    linear1 = linear(reshaped, 512, 'linear1')
+    linear2 = linear(linear1, 1024, 'linear2')
+    linear3 = linear(linear2, 512, 'linear3')
+
+    # linear layer(WX + b),
+    # We don't apply softmax here because
+    # tf.nn.sparse_softmax_cross_entropy_with_logits accepts the unscaled logits
+    # and performs the softmax internally for efficiency.
+    softmax_linear = linear(linear3, NUM_CLASSES, 'softmax_linear')
+
+  return softmax_linear, None
+
+
+def loss(logits, labels, batch_size=None, aux_loss=False):
+  """Add L2Loss to all the trainable variables.
+  Add summary for "Loss" and "Loss/avg".
+  Args:
+    logits: Logits from inference().
+    labels: Labels from distorted_inputs or inputs(). 1-D tensor
+            of shape [batch_size]
+  Returns:
+    Loss tensor of type float.
+  """
+  logits = logits[0]
+
+  # Calculate the average cross entropy loss across the batch.
+  labels = tf.cast(labels, tf.int64)
+  cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(
+      labels=labels, logits=logits, name='cross_entropy_per_example')
+  cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
+  tf.add_to_collection(LOSSES_COLLECTION, cross_entropy_mean)
+
+  # The total loss is defined as the cross entropy loss plus all of the weight
+  # decay terms (L2 loss).
+  # return tf.add_n(tf.get_collection(LOSSES_COLLECTION), name='total_loss')
+
+
+def _add_loss_summaries(total_loss):
+  """Add summaries for losses in CIFAR-10 model.
+  Generates moving average for all losses and associated summaries for
+  visualizing the performance of the network.
+  Args:
+    total_loss: Total loss from loss().
+  Returns:
+    loss_averages_op: op for generating moving averages of losses.
+  """
+  # Compute the moving average of all individual losses and the total loss.
+  loss_averages = tf.train.ExponentialMovingAverage(0.9, name='avg')
+  losses = tf.get_collection(LOSSES_COLLECTION)
+  loss_averages_op = loss_averages.apply(losses + [total_loss])
+
+  # Attach a scalar summary to all individual losses and the total loss; do the
+  # same for the averaged version of the losses.
+  for l in losses + [total_loss]:
+    # Name each loss as '(raw)' and name the moving average version of the loss
+    # as the original loss name.
+    tf.summary.scalar(l.op.name + ' (raw)', l)
+    tf.summary.scalar(l.op.name, loss_averages.average(l))
+
+  return loss_averages_op
+
+
+def train(total_loss, global_step):
+  """Train CIFAR-10 model.
+  Create an optimizer and apply to all trainable variables. Add moving
+  average for all trainable variables.
+  Args:
+    total_loss: Total loss from loss().
+    global_step: Integer Variable counting the number of training steps
+      processed.
+  Returns:
+    train_op: op for training.
+  """
+  # Variables that affect learning rate.
+  num_batches_per_epoch = NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / FLAGS.batch_size
+  decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+
+  # Decay the learning rate exponentially based on the number of steps.
+  lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE,
+                                  global_step,
+                                  decay_steps,
+                                  LEARNING_RATE_DECAY_FACTOR,
+                                  staircase=True)
+  tf.summary.scalar('learning_rate', lr)
+
+  # Generate moving averages of all losses and associated summaries.
+  loss_averages_op = _add_loss_summaries(total_loss)
+
+  # Compute gradients.
+  with tf.control_dependencies([loss_averages_op]):
+    opt = tf.train.GradientDescentOptimizer(lr)
+    grads = opt.compute_gradients(total_loss)
+
+  # Apply gradients.
+  apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
+
+  # Add histograms for trainable variables.
+  for var in tf.trainable_variables():
+    tf.summary.histogram(var.op.name, var)
+
+  # Add histograms for gradients.
+  for grad, var in grads:
+    if grad is not None:
+      tf.summary.histogram(var.op.name + '/gradients', grad)
 
 
 def inference(images, num_classes=NUM_CLASSES, for_training=False,
